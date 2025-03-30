f90fc723fdd889985aefbce549d4bb3f9b297f45
Aly-Taha <aly.taha@aucegypt.edu>
2025-03-30 21:03:35 +0200

    Merge remote changes

---
+++ calculator.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+
+using namespace std;
+
+
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+
+#endif
